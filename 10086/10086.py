f8a7f21dbb4e36e0ece640aa692ac9d4d3d49288
wmm1996528 <912594746@qq.com>
2018-04-02 14:40:25 +0800

    weibo success

---
+++ 10086/10086.py
@@ -0,0 +1,96 @@
+import requests
+import time
+import random
+import urllib.parse
+import base64
+import rsa
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from PIL import Image
+
+class yidongLogin():
+	def __init__(self,username,password):
+		self.username = username
+		self.password = password
+		self.headers= headers = {
+				'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36',
+				'Host':'login.10086.cn',
+				'Referer':'https://login.10086.cn/html/window/loginMini.html?channelID=12003&backUrl=http://shop.10086.cn/i/sso.html'
+		}
+		#持久化session
+		self.session = requests.session()
+
+	def get_login_captcha(self):
+        #发送短信验证码
+		self.session.get('http://shop.10086.cn/i/v1/auth/loginfo?_=' + str(int(time.time() * 1000)), headers=self.headers)
+		indexUrl = 'http://shop.10086.cn/i/?f=home'
+		res2 = self.session.get(indexUrl, headers=self.headers)
+		randomUrl = 'https://login.10086.cn/sendRandomCodeAction.action'
+		randomData = {
+			'userName': self.username,
+			'type': '01',
+			'channelID': '12034'
+		}
+		randomRes = self.session.post(randomUrl, data=randomData, headers=self.headers)
+		if randomRes.text == '0':
+			print('登录验证码发送成功')
+		else:
+			print('发送失败')
+	def get_pwd(self):
+        #公钥 要用encrpyt转换一下
+		pubkey = '''-----BEGIN PUBLIC KEY-----
+			MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgDq4OqxuEisnk2F0EJF
+			mw4xKa5IrcqEYHvqxPs2CHEg2kolhfWA2SjNuGAHxyDDE5MLtOvzuXjBx/5YJtc9
+			zj2xR/0moesS+Vi/xtG1tkVaTCba+TV+Y5C61iyr3FGqr+KOD4/XECu0Xky1W9Zm
+			maFADmZi7+6gO9wjgVpU9aLcBcw/loHOeJrCqjp7pA98hRJRY+MML8MK15mnC4eb
+			ooOva+mJlstW6t/1lghR8WNV8cocxgcHHuXBxgns2MlACQbSdJ8c6Z3RQeRZBzyj
+			fey6JCCfbEKouVrWIUuPphBL3OANfgp0B+QG31bapvePTfXU48TYK0M5kE+8Lgbb
+			WQIDAQAB
+			-----END PUBLIC KEY-----'''
+		rsakey = RSA.importKey(pubkey)
+		cipher = PKCS1_v1_5.new(rsakey)
+		text = base64.b64encode(cipher.encrypt(self.password.encode()))
+		return text.decode()
+
+	def get_login_info(self):
+		self.get_login_captcha()
+		smsPwd = input('请输入验证码:')
+
+		loginUrl = 'https://login.10086.cn/login.htm'
+		loginData = {
+			'accountType': '01',
+			'account': self.username,
+			'password': self.get_pwd(),
+			'pwdType': '01',
+			'smsPwd': smsPwd,
+			'inputCode': '',
+			'backUrl': 'http://shop.10086.cn/i/sso.html',
+			'rememberMe': '0',
+			'channelID': '12034',
+			'protocol': 'https:',
+		}
+
+		loginRes = self.session.get(loginUrl, params=loginData, headers=self.headers)
+		print('denglu jieguo %s ' % loginRes)
+		try:
+			artifact = loginRes.json()['artifact']
+		except Exception as e:
+			print(e)
+		artifactUrl = 'http://shop.10086.cn/i/v1/auth/getArtifact?backUrl=http://shop.10086.cn/i/sso.html&artifact={}'.format(
+			artifact)
+		cookies = {
+			'CmLocation': '100|100',
+			'CmProvid': 'bj',
+			'ssologinprovince': '100'
+		}
+		print(artifactUrl)
+		self.session.get(artifactUrl, headers=self.headers, cookies=cookies)
+
+		infourl = 'http://shop.10086.cn/i/v1/cust/info/' + self.username + '?_=' + str(int(time.time() * 1000))
+		infores = self.session.get(infourl, headers=self.headers, cookies=cookies)
+		print('gerenxinxi %s ' % infores.text)
+
+
+if __name__ == '__main__':
+    login = yidongLogin('15101528779','960128')
+    login.get_login_info()
--- sinaweibo/weibo.py
+++ sinaweibo/weibo.py
@@ -8,6 +8,7 @@ import urllib
 import base64
 class Launcher():
     def __init__(self,username,password):
+        #初始化 用户名 密码 session headers
         self.username = username
         self.password = password
         self.session =  requests.session()
@@ -15,20 +16,20 @@ class Launcher():
             'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
         }
     def pubkeyData(self):
+        #获取 pubkey 数据
         url = 'https://login.sina.com.cn/sso/prelogin.php?entry=account&callback=pluginSSOController.preloginCallBack&su=MTUxMDE1Mjg3Nzk%3D&rsakt=mod&checkpin=1&client=ssologin.js(v1.4.19)&_='+str(int(time.time()*1000))
         req = self.session.get(url,headers=self.headers)
         data = json.loads(re.findall('preloginCallBack\(({.+})',req.text)[0])
-        print(data)
         return data
 
     def get_pwd(self, data):
+        #加密密码
         rsa_e = int('10001',16)
         pw_string = str(data['servertime']) + '\t' + str(data['nonce']) + '\n' + str(self.password)
         key = rsa.PublicKey(int(data['pubkey'], 16), rsa_e)
         pw_encypted = rsa.encrypt(pw_string.encode('utf-8'), key)
         self.password = ''  # 安全起见清空明文密码
         passwd = binascii.b2a_hex(pw_encypted)
-        print(passwd)
         return passwd
 
     def get_su(self):
@@ -38,6 +39,7 @@ class Launcher():
     def login(self):
         url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.19)'
         data = self.pubkeyData()
+        #构造 post数据 
         post_data = {
             "entry": "weibo",
             "gateway": "1",
@@ -60,20 +62,23 @@ class Launcher():
             "url": "https://weibo.com/ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack",
             "returntype": "META"
         }
-        print(post_data)
+
         res = self.session.post(url,data=post_data,headers=self.headers)
         # print(res.text)
+        #post 后会返回 重定向页面的网址
         url2 = re.findall('location\.replace\("(https://.+)"\);',res.content.decode('gbk'))[0]
-        print(url2)
         res2 = self.session.get(url2,headers=self.headers)
         url3 = re.findall("location\.replace\('(https://.+)'\);",res2.content.decode('gbk'))[0]
-        print(url3)
         res3 = self.session.get(url3,headers=self.headers)
+        #重定向结束后 登录成功 访问你的首页 通过判断你的用户名是否存在来认定是否登录成功
         loginurl = 'http://weibo.com'+re.findall('"userdomain":"(.+)}}',res3.text)[0]
         loginres = self.session.get(loginurl,headers=self.headers).text
-        print(loginres)
-
-
+        try:
+            username = re.findall(r"\$CONFIG\['nick'\]='(.+); ", loginres)[0]
+        except Exception as e:
+            print(e)
+        if username:
+            print('登录成功 当前用户为；%s' % username)
 
 
 if __name__ == '__main__':
